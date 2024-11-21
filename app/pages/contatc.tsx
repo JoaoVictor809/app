6c8d4a0c5da5c1dbdac3ac39addc29afec8a4b2d
JoaoVictor809 <joaovictorleal.mello@outlook.com>
2024-11-20 23:31:08 -0300

    Add files via upload

--- app/pages/contatc.tsx
+++ app/pages/contatc.tsx
@@ -5,42 +5,33 @@ import Estilo from "../../assets/style/contatc";
 export default function contatc() {
  
   return (
-    <View style={Estilo.containerMain}>
-        <Text style={Estilo.title}><h2>CONTATOS</h2></Text>
-        {/* icones do contato */}
-        <View>
-            {/* telefone */}
-        <View style={Estilo.containerImg}>
-            <Image source={require('../../assets/images/tell.png')} />
-            <Text style={Estilo.text}>-Telefone:(11) 4163-4655</Text>
-        </View>
-        {/* Local */}
-        <View style={Estilo.containerImg}>
-            <Image source={require('../../assets/images/local.png')} />
-            <Text style={Estilo.text}>-Endereço: R. João Batista Soares, 440 - Centro, Barueri - SP, 06401-135</Text>
-        </View>
-        {/* celular */}
-        <View style={Estilo.containerImg}>
-            <Image source={require('../../assets/images/cell.png')} />
-            <Text style={Estilo.text}>-Celular: +55 114163-4659</Text>
-        </View>
-        {/* email */}
-        <View style={Estilo.containerImg}>
-            <Image source={require('../../assets/images/email.png')} />
-            <Text style={Estilo.text}>-Email: e245.secretaria@etec.sp.gov.br</Text>
-        </View>
-        {/* insta */}
-        <View style={Estilo.containerImg}>
-            <Image source={require('../../assets/images/insta.png')} />
-            <Text style={Estilo.text}>-Insta: @etecantoniofurlan</Text>
-        </View>
-        {/* site */}
-        <View style={Estilo.containerImg}>
-            <Image source={require('../../assets/images/web.png')} />
-            <Text style={Estilo.text}>-Site: https://www.etecantoniofurlan.com.br/</Text>
-        </View>
+   <View style={{flex:1,width:"100%", backgroundColor:"#fff"}}>
+    <View style={Estilo.container}>
+        <Text><h1 style={{color:"#fff"}}>Contatos</h1></Text>
+    </View>
+      <View style={Estilo.imagem}>
+        <Image source={require('../../assets/images/contatos.png')}/>
+      </View>
+      <View style={Estilo.containerMain}>
+        <View style={Estilo.container01}>
+            <Text><h2 style={{color:"#293B91"}}>(11) 4163-4655</h2></Text>
+            <Image source={require('../../assets/images/tell.png')}/>
         </View>
 
-        
-    </View>
+        <View style={Estilo.container02}>
+        <Image source={require('../../assets/images/email.png')}/>
+            <Text><h2 style={{color:"#293B91"}}>e245.secretaria@etec.sp.gov.br</h2></Text>
+        </View>
+
+        <View style={Estilo.container01}>
+            <Text><h2 style={{color:"#293B91"}}>@etecantoniofurlan</h2></Text>
+            <Image source={require('../../assets/images/insta.png')}/>
+        </View>
+
+        <View style={Estilo.container02}>
+        <Image source={require('../../assets/images/local.png')}/>
+            <Text><h2 style={{color:"#293B91"}}>R. João Batista Soares, 440<br />Centro, Barueri - SP, 06401-135</h2></Text>
+        </View>
+      </View>
+   </View>
   )}
